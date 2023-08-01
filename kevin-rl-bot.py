4a2e51df1a6897d1b0c4c4c59e4873aa812bbc12
KevinRomero28 <140850983+KevinRomero28@users.noreply.github.com>
2023-07-31 21:44:45 -0500

    updates

--- kevin-rl-bot.py
+++ kevin-rl-bot.py
@@ -11,26 +11,48 @@ class Bot(GoslingAgent):
         my_location_to_foe_location=abs(self.me.location.y-self.foe_goal.location.y)
         is_in_front_of_ball = ball_to_foe_goal_y > my_location_to_foe_location
         self_location_to_friend_goal_right_post=abs((self.me.location - self.friend_goal.right_post).magnitude())
-        D4=(self.me.location.z-self.ball.location.z)
-        # foe.location from me
-        #foe.lopcation from the_ball 
-        # friend.location_from_the_ball 
+        self_location_to_ball_location_z=(self.me.location.z-self.ball.location.z)
+        #foe_location_from_me=
+        #foe_location_from_the_ball= 
+        friend_location_from_the_ball=self.me.location-self.ball.location 
 
         if self.kickoff_flag:
             self.set_intent(kickoff())
             return
         
-        if self_location_to_friend_goal_right_post > 1000:
+        def run(self,agent):
+            target= agent.ball.location + Vector3(0,200*side(agent.team),0)
+            local_target = agent.me.local(target-agent.me.location)
+            defaultPD(agent,local_target)
+            defaultThrottle(agent,2300)
+            if local_target.magnitude() < 650:
+                agent.set_intent(
+                    flip(agent.me.local(agent.foe_goal.location - agent.me.location)))
+                if self.me.boost > 80:
+                    self.set_intent(short_shot(self.foe_goal.location))
+            return
+                
+        
+        #if self_location_to_friend_goal_right_post > 1000:
             self.set_intent(goto(self.ball.location))
             targets ={'at_opponent_goal':(self.foe_goal.left_post, self.foe_goal_post), 
             'away_from_our_net':(self.friend_goal.right_post,self.friend_goal.left_post)}
             hits= find_hits(self, targets)
 
+            if len(hits['at_opponent_goal']) > 0:
+                self.set_intent(hits['at_opponent_goal'])
+                
+            
+            if len(hits['away_from_our_net']) > 0:
+                self.set_intent(hits['away_from_our_net'])
+                
+
             return
         
-        if is_in_front_of_ball or self.me.location :
-            self.set_intent(goto(self.friend_goal.right_post,self.friend_goal.left_post))
-            return 
+        #if is_in_front_of_ball or self.me.location > my_location_to_foe_location:
+            self.set_intent(hits)['at_opponent_goal'] > 0
+                
+    
 
 
         if self.me.boost > 80:
@@ -47,6 +69,6 @@ class Bot(GoslingAgent):
         if closest_boost is not None:
             self.set_intent(goto(closest_boost.location))
             return
-        self.set_intent(jump_shot)
+        self.set_intent(short_shot(self.foe_goal.location))
         
             
